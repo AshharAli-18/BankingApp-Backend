10a56cdb091b124967c4140d6b7057e708318741
AshharAli-18 <iashharali.18@gmail.com>
2024-08-05 14:24:04 +0500

    updated

--- src/main/java/com/Training/BankingApp/deletedaccount/DeletedAccountController.java
+++ src/main/java/com/Training/BankingApp/deletedaccount/DeletedAccountController.java
@@ -1,8 +1,39 @@
-//package com.Training.BankingApp.deletedaccount;
-//
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class DeletedAccountController {
-//
-//}
+package com.Training.BankingApp.deletedaccount;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeletedAccountController {
+
+    @Autowired
+    DeletedAccountService deletedAccountService;
+
+    @GetMapping("/api/getAllDeletedAccounts")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<DeletedAccount> getAllDeletedAccounts(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                        @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        return deletedAccountService.getAllDeletedAccounts(page, size);
+    }
+    @DeleteMapping("/api/admin/permanentAccountDelete/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> permanentDeleteAccount(@PathVariable Long id) {
+        try {
+            deletedAccountService.permanentlyDeleteAccount(id);
+            return ResponseEntity.ok("Account successfully deleted");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+        }
+    }
+
+
+}
