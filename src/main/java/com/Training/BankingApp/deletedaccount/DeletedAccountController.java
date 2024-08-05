dbb925b5f1294e5923432e313c06372c35254e01
AshharAli-18 <iashharali.18@gmail.com>
2024-08-05 10:59:20 +0500

    updated

---
+++ src/main/java/com/Training/BankingApp/deletedaccount/DeletedAccountController.java
@@ -0,0 +1,8 @@
+//package com.Training.BankingApp.deletedaccount;
+//
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class DeletedAccountController {
+//
+//}
---
+++ src/main/java/com/Training/BankingApp/deletedaccount/DeletedAccountService.java
@@ -0,0 +1,10 @@
+//package com.Training.BankingApp.deletedaccount;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class DeletedAccountService {
+//    @Autowired
+//    private
+//}
